79204d943e2cf8e18215d8b038a3619b5a7bb758
nickschmitt <nickschmitt@gmail.com>
2021-07-16 17:16:35 -0700

    Create Javascript file to execute generated Wasm module
    wat2wasm helloworld.wat generates a wasm file, which we now can
    call with a Js file.
    
    We read the wasm file with readFileSync, and declare variables
    pertaining to the helloWorld function to be called, the
    startStringIndex pertaining to the memory location to be used,
    and memory pertaining to the 64kb single-page linear Wasm memory
    to be used.
    
    Next we store these in an import object, with a top-level property
    of env as expected by the Wasm file, within which we have the
    expected buffer of memory, the start_string memory location and
    the print_string function that creates an array of memory using
    the buffer, the byteOffset and the length (see MDN).
    
    This is then passed to a TextDecoder which decodes the buffer
    into a string to be console logged.
    
    Finally, the wasm code is instiantiated along with the import
    object and all its properties matching the values to be imported
    into the Wasm instance.
    
    With this, we grab the helloworld property from the instance
    exports and assign it to the Javascript variable, which we
    can then call, successfully console logging hello world.

---
+++ helloworld.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const bytes = fs.readFileSync(__dirname + '/helloworld.wasm')
+
+let helloWorld = null
+let startStringIndex = 100
+let memory = new WebAssembly.Memory({ initial: 1 })
+
+let importObject = {
+	env: {
+		buffer: memory,
+		start_string: startStringIndex,
+		print_string: function (strLen){
+			const bytes = new Uint8Array (memory.buffer, startStringIndex, strLen)
+			const logString = new TextDecoder('utf8').decode(bytes)
+			console.log(logString)
+		}
+	}
+};
+
+(async()=>{
+	let obj = await WebAssembly.instantiate(new Uint8Array(bytes), importObject);
+	({helloworld: helloWorld} = obj.instance.exports);
+	helloWorld();
+})();
